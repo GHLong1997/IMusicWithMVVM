23bcf4ccaebdd60f674c45ccc30d632323cdf043
Long <15i1.hangoclong@gmail.com>
2018-04-27 08:39:04 +0700

    init base project

---
+++ app/src/main/java/com/example/along/scmusic/screen/BaseViewModel.java
@@ -0,0 +1,8 @@
+package com.example.along.scmusic.screen;
+
+public abstract class BaseViewModel {
+
+    protected abstract void onStart();
+
+    protected abstract void onStop();
+}
--- app/src/main/java/com/example/along/scmusic/screen/main/MainActivity.java
+++ app/src/main/java/com/example/along/scmusic/screen/main/MainActivity.java
@@ -1,7 +1,8 @@
-package com.example.along.scmusic;
+package com.example.along.scmusic.screen.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.along.scmusic.R;
 
 public class MainActivity extends AppCompatActivity {
 
